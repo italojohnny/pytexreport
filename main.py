80dfbb5c2a6ea1a9c70b97f2c6c92ac784a0dd39
italojohnny <italojohnnydosanjos@gmail.com>
2016-12-02 17:00:41 -0200

    update

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 #!/usr/python3.5
+import psycopg2
+import psycopg2.extras
 
 class pytex_report (object):
-    def __init__ (self):
-        pass
+    def __init__ (self, banco='', usuario='', senha='', ip='192.168.100.253', porta='5432'):
+        self.conexao = psycopg2.connect(database=banco, user=usuario, password=senha, host=ip, port=porta)
+
     def __del__ (self):
-        pass
-    def testando (self):
-        dados = [{'nome':'italo', 'sobrenome':'johnny', 'nascimento': '23/03/1989', 'profissao':'programador', 'endereco':'rua dos bobos', 'bairro': 'jardim luiza', 'cidade':'franca', 'estado':'sao paulo'}]
+        self.conexao.close()
 
-        cabecalho = r'''
-        \begin{longtable}{
-        p{.1\textwidth}
-        p{.1\textwidth}
-        p{.1\textwidth}
-        p{.1\textwidth}
-        p{.1\textwidth}
-        p{.1\textwidth}
-        p{.1\textwidth}
-        p{.1\textwidth}
-        p{.1\textwidth}
-        }
-        '''
-        rodape = r'''
-        \end{longtable}
-        '''
-        print(cabecalho)
-        print("%s & %s & %s & %s & %s & %s & %s & %s \\\\ \\hline" % ('nome', 'sobrenome', 'nascimento', 'profissao', 'endereco', 'bairro', 'cidade', 'estado'))
-        print(r"\endhead")
-        for j in range(0, 300):
-            for i in dados:
-                print("%s & %s & %s & %s & %s & %s & %s & %s \\\\ \\hline" % (i['nome'], i['sobrenome'], i['nascimento'], i['profissao'], i['endereco'], i['bairro'], i['cidade'], i['estado']))
-        print(rodape)
-
+    def exec_query (self, query):
+        cursor = self.conexao.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+        cursor.execute(query)
+        return cursor.fetchall()
 
 def main ():
-    pytex_report().testando()
+    results = pytex_report().exec_query("select * from f_bilhetes_chamadas order by horario desc limit 100")
+    print(r"\begin{longtable}{ *4{p{0.24\textwidth}} }")
+
+    print(r"\multicolumn{4}{c}{Relatorio}\\ \hline")
+    print(r"%s & %s & %s & %s \\ \hline" % ('origem', 'destino', 'horario', 'status'))
+    print(r"\endfirsthead")
+    print(r"\hline")
+
+    print(r"%s & %s & %s & %s \\ \hline" % ('origem', 'destino', 'horario', 'status'))
+    print(r"\endhead")
+    print(r"\hline")
+
+    for i in results:
+        print(r"%s & %s & %s & %s \\ \hline" % (i['origem'], i['destino'], i['horario'], i['status']))
+
+    #print(r"\hline")
+    #print(r"\multicolumn{4}{c}{Relatorio}\\ \hline")
+    #print(r"\endfoot")
+    print(r"\hline")
+
+    print(r"\endlastfoot")
+    print(r"\multicolumn{4}{c}{Relatorio}\\")
+
+    print(r"\end{longtable}")
 
 if __name__ == "__main__": main()
-#python main.py > tmp.tex && make
+
+#http://tex.stackexchange.com/questions/42606/overfull-warning-when-using-longtable
