5d9478e1edfb2f40b9a728940d338f457cfbc404
italojohnny <italojohnnydosanjos@gmail.com>
2016-12-02 22:16:34 -0200

    one more swift update

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 #!/usr/python3.5
 import psycopg2
 import psycopg2.extras
+import re
 
 class pytex_report (object):
-    def __init__ (self, banco='', usuario='', senha='', ip='192.168.100.253', porta='5432'):
-        self.conexao = psycopg2.connect(database=banco, user=usuario, password=senha, host=ip, port=porta)
+    def __init__ (self, sql):
+        self.ip      = '192.168.1.104'
+        self.banco   = 'bancodedados'
+        self.usuario = 'admteste'
+        self.senha   = 'yma2578k'
+        self.porta   = '5432'
+        self.conexao = psycopg2.connect(database=self.banco, user=self.usuario, password=self.senha, host=self.ip, port=self.porta)
+
+        self.resultado = self.exec_query(sql)
+        if self.resultado:
+            self.tamanho = len(self.resultado[0])
+            self.keys = [x for x in self.resultado[0].keys()]
+        else:
+            self.tamanho = 0
+            self.keys = []
+        self.iterador = self.iterador_registro()
+        self.tabela = self.relatorio()
 
     def __del__ (self):
         self.conexao.close()
@@ -14,31 +30,33 @@ class pytex_report (object):
         cursor.execute(query)
         return cursor.fetchall()
 
+    def iterador_registro (self):
+        texto = "%s"
+        for i in range(self.tamanho -1):
+            texto += " & %s"
+        texto += r" \\ \hline"
+        return texto
+
+    def trata_texto_tex (self, texto):
+        return re.sub(r"(_)", r"\\\1", texto)
+
+    def relatorio (self):
+        tabela = r"\begin{longtable}{ *%s{p{0.24\textwidth}} }" % self.tamanho
+
+        tabela+="\n"
+        tabela+= self.iterador % tuple([self.trata_texto_tex(x) for x in self.keys])
+        tabela+="\n\\endhead"
+
+        for i in self.resultado:
+            tabela+="\n"
+            tabela+= self.iterador % tuple([self.trata_texto_tex(str(i[x])) for x in self.keys])
+
+        tabela+= "\n"
+        tabela+= r"\end{longtable}"
+        return tabela
+
 def main ():
-    results = pytex_report().exec_query("select * from f_bilhetes_chamadas order by horario desc limit 100")
-    print(r"\begin{longtable}{ *4{p{0.24\textwidth}} }")
-
-    print(r"\multicolumn{4}{c}{Relatorio}\\ \hline")
-    print(r"%s & %s & %s & %s \\ \hline" % ('origem', 'destino', 'horario', 'status'))
-    print(r"\endfirsthead")
-    print(r"\hline")
-
-    print(r"%s & %s & %s & %s \\ \hline" % ('origem', 'destino', 'horario', 'status'))
-    print(r"\endhead")
-    print(r"\hline")
-
-    for i in results:
-        print(r"%s & %s & %s & %s \\ \hline" % (i['origem'], i['destino'], i['horario'], i['status']))
-
-    #print(r"\hline")
-    #print(r"\multicolumn{4}{c}{Relatorio}\\ \hline")
-    #print(r"\endfoot")
-    print(r"\hline")
-
-    print(r"\endlastfoot")
-    print(r"\multicolumn{4}{c}{Relatorio}\\")
-
-    print(r"\end{longtable}")
+    print(pytex_report("select * from tabela1").tabela)
 
 if __name__ == "__main__": main()
 
